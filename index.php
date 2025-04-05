b180f59d768743da07a5bdd922d2f730f38a30d4
Andres <afguevarac@ufpso.edu.co>
2025-04-05 14:27:44 -0500

    Nuevos cambios

--- index.php
+++ index.php
@@ -1,32 +1,42 @@
-<?php
+<?php 
 
-    $servidor = "localhost";
-    $usuario = "root";
-    $clave = "";
-    $baseDeDatos = "gocarg";
+    $servidor="localhost";
+    $usuario="root";
+    $clave="";
+    $baseDeDatos="gocarg";
 
-    $enlace = mysqli_connect($servidor, $usuario, $clave, $baseDeDatos);
+    $enlace= mysqli_connect($servidor,$usuario,$clave, $baseDeDatos);
 
-
-?>  
-<?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["registro"])) {
-        /*echo "<pre>";
-         print_r($_POST);
-         "</pre>";
-         */
-        $nombre = isset($_POST["Nombre"]) ? $_POST["Nombre"] : "";
-        $email = isset($_POST["email"]) ? $_POST["email"] : "";
-        $contraseña = isset($_POST["contraseña"]) ? $_POST["contraseña"] : "";
-    
-        if (!empty($nombre) && !empty($contraseña && !empty($email))) {
-            $insertarDatos = "INSERT INTO clientes (nombre,email,contraseña) VALUES ('$nombre','$email','$contraseña')";
-            $ejecutarInsertarDatos = mysqli_query($enlace, $insertarDatos);
-
-            echo"Te has registrado Exitosamente vuele atras e inicia sesion";
-    
-        }
-    }
-    
 ?>
+<?php
 
+    if($_SERVER["REQUEST_METHOD"]=="POST" && isset($_POST["registro"])){
+
+        $nombre=isset($_POST["Nombre"]) ? $_POST["Nombre"]:"";
+        $email=isset($_POST["email"]) ? $_POST["email"]:"";
+        $contraseña=isset($_POST["Contraseña"])? $_POST["Contraseña"]:"";
+        $tabla=isset($_POST["Tipo_persona"]) ? $_POST["Tipo_persona"]:"conductor";
+
+        $placa=isset($_POST["placa"])? $_POST["placa"]:"";
+        $vehiculo=isset($_POST["vehiculo"])? $_POST["vehiculo"]:"";
+
+        if(!empty($nombre)&& !empty($email) && !empty($contraseña)){
+            if(!in_array($tabla,["conductor","cliente"])){
+                die("Tabla no encontrada");
+            }
+
+            if($tabla == "conductor"){
+                $insertarDatos="INSERT INTO conductores (Nombre,Email,Contraseña,TipoVehiculo,PlacaVehiculo) VALUES ('$nombre','$email','$contraseña','$vehiculo','$placa')";
+                $ejecutarInsertarDatos= mysqli_query($enlace,$insertarDatos);
+            }
+
+            if($tabla == "cliente"){
+                $insertarDatos= "INSERT INTO clientes (nombre,email,contraseña) VALUES ('$nombre','$email','$contraseña')";
+                $ejecutarInsertarDatos=mysqli_query($enlace,$insertarDatos);
+            }
+
+            echo "Registrado con exito ahora vuelve al inicio e inicia sesion";
+        } 
+    }
+
+?>
